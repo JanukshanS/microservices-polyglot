6a058ab0397b2171543982bbea8b6b9f48296d9c
JanukshanS <sjanukshan9825@gmail.com>
2026-03-03 00:39:00 +0530

    fixed payment service issue

--- payment-service/app.py
+++ payment-service/app.py
@@ -1,8 +1,9 @@
+import itertools
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 payments = []
-id_counter = 1
+id_counter = itertools.count(1)
 
 @app.route('/payments', methods=['GET'])
 def get_payments():
@@ -10,11 +11,9 @@ def get_payments():
 
 @app.route('/payments/process', methods=['POST'])
 def process_payment():
-    global id_counter
     payment = request.json
-    payment['id'] = id_counter
+    payment['id'] = next(id_counter)
     payment['status'] = 'SUCCESS'
-    id_counter += 1
     payments.append(payment)
     return jsonify(payment), 201
 
@@ -26,4 +25,4 @@ def get_payment(id):
     return "Not found", 404
 
 if __name__ == '__main__':
-    app.run(port=8083)
+    app.run(host='0.0.0.0', port=8083)
